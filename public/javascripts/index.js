3a9a97f9b0c368e04feac2544a80b70ee18a7812
Owen van Dijk <owen.vandijk@192.168.1.109>
2015-06-14 00:37:17 +0100

    adding event handlers

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+/* TODO convert to Angular */
+document.addEventListener('load', function(){
+
+  initialize: function() {
+    console.log('initialize');
+  }
+
+  addEvents: function() {
+    console.log("addEvents");
+  }
+
+  initialize();
+
+});
