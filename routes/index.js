3c39a9c8926c2b2aad6b022e6f6eea0a593d7871
Owen van Dijk <owen.vandijk@LON-M-T21782-9.local>
2015-06-13 20:16:53 +0100

    switch

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET example.txt */
-router.get('/example.txt', function(req, res, next) {
-  res.sendfile('example.txt');
-});
-
 module.exports = router;
