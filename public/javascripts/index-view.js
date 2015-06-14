7be849c8f1b3581e34a8d9f4e0a49896b22bfff7
Owen van Dijk <owen.vandijk@192.168.1.109>
2015-06-14 01:03:54 +0100

    renamed index.js

---
+++ public/javascripts/index-view.js
@@ -0,0 +1,15 @@
+/* TODO convert to Angular */
+document.addEventListener('load', function(){
+
+  /*
+  initialize: function() {
+    console.log('initialize');
+  }
+
+  addEvents: function() {
+    console.log("addEvents");
+  }
+
+  initialize();*/
+
+});
