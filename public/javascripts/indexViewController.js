057503c753491207434a46c7e398463aee61d1ad
Owen van Dijk <owen.vandijk@LON-M-T21782-9.local>
2015-06-14 19:01:22 +0100

    css fixxes

--- public/javascripts/indexViewController.js
+++ public/javascripts/indexViewController.js
@@ -1,17 +1,11 @@
 /* TODO convert to Angular */
 window.addEventListener('load', function(){
 
-  /*
-  var initialize: function() {
-    console.log('initialize');
+  var initialize = function() {
+    console.log("test");
+
   }
 
-  addEvents: function() {
-    console.log("addEvents");
-  }
-
-  initialize();*/
-
-  console.log("test");
+  initialize();
 
 });
