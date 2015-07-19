1c3fe6a58f306de4b34eedaf3bea15161c5c54a8
Christopher Webb <christopher.webb@greenmangaming.com>
2015-07-19 16:12:55 +0100

    Further payment form
    Further fleshed out the credit card for.

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexController);
 app.use('/', inviteFriendsController);
+app.use('/', createPaymentAndSendInviteController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/createPaymentAndSendInviteController.js
+++ routes/createPaymentAndSendInviteController.js
@@ -2,8 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/invite', function(req, res, next) {
-  res.render('invite', { title: 'Invite to' });
+router.get('/payment', function(req, res, next) {
+  res.render('payment', {
+    title: 'Invite to',
+    exp_months: [
+      { "month": "January" },
+      { "month": "Febuary" },
+      { "month": "March" },
+      { "month": "April" },
+      { "month": "May" },
+      { "month": "June" },
+      { "month": "July" },
+      { "month": "August" },
+      { "month": "September" },
+      { "month": "October" },
+      { "month": "November" },
+      { "month": "December" },
+    ],
+    exp_years: [
+      2015,
+      2016,
+      2017,
+      2018,
+      2019,
+      2020,
+      2021,
+      2022,
+      2023,
+      2024,
+      2025,
+      2026,
+      2027,
+      2028,
+      2029,
+      2030,
+      2031,
+      2032,
+      2033,
+      2034,
+    ]
+  });
 });
 
 module.exports = router;
