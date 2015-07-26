a370e398542ee7015cc4ee031fe6a633c87d5168
Christopher Webb <christopher.webb@greenmangaming.com>
2015-07-26 16:48:44 +0100

    Session stores the fake details

--- routes/inviteFriendsController.js
+++ routes/inviteFriendsController.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/invite/:invite_token', function(req, res, next) {
-  res.render('inviteFriends', { title: 'Invite friends' });
+  var invite_token = req.params.invite_token
+  var gig_url = req.session.searches[invite_token];
+  console.log('found gig! for token ' + invite_token + ': ' + gig_url);
+  res.render('inviteFriends', {
+    title: 'Invite friends',
+    gig: gig_url
+  });
 });
 
 module.exports = router;
--- routes/searchController.js
+++ routes/searchController.js
@@ -3,23 +3,26 @@ var router = express.Router();
 
 /* Results for search. */
 router.get('/search/:search_for', function(req, res, next) {
-  console.log('User searched for ' + req.search_for);
-  var index = 0;
+  var search_for = req.params.search_for;
 
-  if (req.session.searches) {
-    index = req.session['searches'].push(req.search_for);
-  } else {
-    req.session.searches = [req.search_for];
-  }
-
-  res.json({
-    result_token: index,
-    gig: 'GiggyPOP launch party',
+  var search_result = {
+    gig: search_for,
     date: 1440187200,
     venue: 'Your fave place',
     venue_loc: 'With your Giggy Buddies!',
     img_url: ''
-  });
+  };
+  var index = 0;
+
+  if (req.session.searches) {
+    index = req.session['searches'].push(search_for) - 1;
+  } else {
+    req.session.searches = [search_for];
+  }
+
+  search_result['result_token'] = index;
+
+  res.json(search_result);
 });
 
 module.exports = router;
