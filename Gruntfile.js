1b7e0354e8973dc7f9a1ddb1a3721093360edc26
Nick Skelton <mail@beardscratchers.com>
2015-08-06 22:47:59 +0100

    Added Gruntfile. New grunt tasks for compass compilation/watch. Updated path of compiled css. Tidied gitignore.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		compass: {
+			options: {
+				sassDir: 'public/scss/',
+				cssDir: 'public/stylesheets/',
+				bundleExec: true
+			},
+			build: {
+				options: { outputStyle: 'compressed' }
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	grunt.registerTask('default', ['watch']);
+
+};
